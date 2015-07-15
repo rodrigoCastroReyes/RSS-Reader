a0f8af7583c7c7033e92d3e3813b979b890c4b73
Angely Oyola <angelybe_be13@hotmail.com>
2015-07-15 13:01:53 -0500

    Ingresando informaciòn de nuevos proveedores en ventana de nuevo proveedor

--- VentanaProveedor.py
+++ VentanaProveedor.py
@@ -9,6 +9,8 @@
 
 from PyQt4 import QtCore, QtGui
 import sys
+from CargaRss import *
+import json
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -27,8 +29,10 @@ except AttributeError:
 class VentanaProveedor(QtGui.QDialog):
 
     def __init__(self, parent = None):
+        self.newsProviders= []
         super(VentanaProveedor, self).__init__(parent)
         self.setupUi(self)
+
 
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
@@ -64,13 +68,31 @@ class VentanaProveedor(QtGui.QDialog):
         self.pushButton = QtGui.QPushButton(Form)
         self.pushButton.setGeometry(QtCore.QRect(190, 240, 98, 27))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
-
         self.retranslateUi(Form)
+        self.loadNewProviders()
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Agregar Proveedor", None))
         self.pushButton.setText(_translate("Form", "OK", None))
+
+#Permite cargar lista de nuevos proveedores para que el usuairo se pueda suscribir
+
+    def loadNewProviders(self):#leeer el archivo ubicado en filePath y crea los hilos en base a esa informacion
+        with open("newsProviders.json", encoding='utf-8') as data_file:
+            data = json.loads(data_file.read())
+        self.time=int(data['time'])
+        providers=data['threads']
+        for dataProv in providers:
+            self.prov = Provider(dataProv["id"], dataProv["name"], dataProv["url"], int(dataProv["maxFeeds"]))
+            self.newsProviders.append(self.prov)
+        self.printNewsProviders()
+
+
+    def printNewsProviders(self):
+        for i in range (1, len(self.newsProviders)):
+            print(self.newsProviders[i-1].printAllProvider())
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
