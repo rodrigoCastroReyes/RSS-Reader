d25b75a97632d63e7211564d1a91729c5fc2b8ed
Jhon <jjbarrei@espol.edu.ec>
2015-07-15 22:51:43 -0500

    Agregada funcionalidad de la ventanaTools

--- designer/vprooo.py
+++ designer/vprooo.py
@@ -2,15 +2,11 @@
 
 # Form implementation generated from reading ui file 'VentanaProveedor.ui'
 #
-# Created: Wed Jun 24 22:20:00 2015
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt4 UI code generator 4.11.4
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-import sys
-from CargaRss import *
-import json
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -26,14 +22,7 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class VentanaProveedor(QtGui.QDialog):
-
-    def __init__(self, parent = None):
-        self.newsProviders= []
-        super(VentanaProveedor, self).__init__(parent)
-        self.setupUi(self)
-
-
+class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
         Form.resize(716, 480)
@@ -68,63 +57,44 @@ class VentanaProveedor(QtGui.QDialog):
         self.scrollAreaWidgetContents.setObjectName(_fromUtf8("scrollAreaWidgetContents"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.checkBox_2 = QtGui.QCheckBox(self.scrollAreaWidgetContents)
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
+        self.horizontalLayout.addWidget(self.checkBox_2)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.checkBox = QtGui.QCheckBox(self.scrollAreaWidgetContents)
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.horizontalLayout_2.addWidget(self.checkBox)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.checkBox_3 = QtGui.QCheckBox(self.scrollAreaWidgetContents)
+        self.checkBox_3.setText(_fromUtf8(""))
+        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
+        self.horizontalLayout_3.addWidget(self.checkBox_3)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
+        self.checkBox_4 = QtGui.QCheckBox(self.scrollAreaWidgetContents)
+        self.checkBox_4.setText(_fromUtf8(""))
+        self.checkBox_4.setObjectName(_fromUtf8("checkBox_4"))
+        self.horizontalLayout_4.addWidget(self.checkBox_4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.verticalLayout.addWidget(self.scrollArea)
-
         self.addButton = QtGui.QPushButton(Form)
         self.addButton.setObjectName(_fromUtf8("addButton"))
         self.verticalLayout.addWidget(self.addButton)
-        self.loadNewProviders()
-        self.showNewsProviders(Form)
-        self.addButton.pressed.connect(self.addButton_clicked)
 
         self.retranslateUi(Form)
-
-        self.listNewFeeds = []
-
-    def addButton_clicked(self):
-        for i in range (1, len(self.checks)):
-            if (self.checks[i-1].isChecked()):
-                print (self.checks[i-1].text()) #Imprime los checbox seleccionados (para verificar)
-                self.listNewFeeds.append(self.checks[i-10].text())
-
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Agregar Proveedor", None))
+        self.checkBox_2.setText(_translate("Form", "checkbox1hahahah", None))
+        self.checkBox.setText(_translate("Form", "checkbox1hahahahmmmmmmmmmmmm", None))
         self.addButton.setText(_translate("Form", "Add", None))
 
-#Permite cargar lista de nuevos proveedores para que el usuairo se pueda suscribir
-
-    def loadNewProviders(self):#leeer el archivo ubicado en filePath y crea los hilos en base a esa informacion
-        with open("newsProviders.json", encoding='utf-8') as data_file:
-            data = json.loads(data_file.read())
-        self.time=int(data['time'])
-        providers=data['threads']
-        for dataProv in providers:
-            self.prov = Provider(dataProv["id"], dataProv["name"], dataProv["url"], int(dataProv["maxFeeds"]))
-            self.newsProviders.append(self.prov)
-        self.printNewsProviders()
-
-    def showNewsProviders(self, Form):
-        #newProviderBOX.setObjectName(_fromUtf8("horizontalLayout_3"))
-        self.checks = []
-        for i in range (1, len(self.newsProviders)):
-            newProviderBOX = QtGui.QHBoxLayout()
-            cB = QtGui.QCheckBox(self.newsProviders[i-1].printAllProvider())
-            #cB.setText(_translate("Form", "%s",self.newsProviders[i-1].printAllProvider())) 
-            self.checks.append(cB)
-            newProviderBOX.addWidget(cB)
-            self.verticalLayout_2.addLayout(newProviderBOX)
-            #print(self.newsProviders[i-1].printAllProvider())
-
-    def printNewsProviders(self):
-        for i in range (1, len(self.newsProviders)):
-            print(self.newsProviders[i-1].printAllProvider())
-
-
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    vp = VentanaProveedor()
-    vp.show()
-    sys.exit(app.exec_())
